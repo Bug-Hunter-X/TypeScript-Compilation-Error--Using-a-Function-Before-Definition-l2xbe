3583d19fb26e942692d9989277a2cc88e0454df5
BugHunter-X <askzenai@gmail.com>
2024-12-27 07:10:05 +0100

    Add bugSolution.ts file

---
+++ bugSolution.ts
@@ -0,0 +1,20 @@
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+function subtract(a: number, b: number): number {
+  return a - b;
+}
+
+function multiply(a: number, b: number): number {
+  return a * b; 
+}
+
+let result = add(5, 3); // result is 8
+console.log(result); // output: 8
+
+result = subtract(5, 3); // result is 2
+console.log(result); // output: 2
+
+result = multiply(5, 3); // result is 15
+console.log(result); // output: 15
